ea71db256eb93887564bfb54d3f313901b04b6e6
Chloë <josiericetong@gmail.com>
2022-07-10 14:11:23 -0700

    can filter by status

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -2,10 +2,13 @@ import axios from "axios";
 import { useQuery } from "react-query";
 import { IssueItem } from "./IssueItem";
 
-export default function IssuesList({ labels }) {
-  const { data, isLoading } = useQuery(["issues", { labels }], () => {
+export default function IssuesList({ labels, status }) {
+  const { data, isLoading } = useQuery(["issues", { labels, status }], () => {
+    const statusString = status ? `&status=${status}` : "";
     const labelsString = labels.map((label) => `labels[]=${label}`).join("&");
-    return axios.get(`/api/issues?${labelsString}`).then((res) => res.data);
+    return axios
+      .get(`/api/issues?${labelsString}${statusString}`)
+      .then((res) => res.data);
   });
 
   return (
